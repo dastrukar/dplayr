37faf8241292660c74b97383f69206f3a31c4caa
dastrukar <3derid@gmail.com>
2021-03-06 18:48:21 +0800

    Main: Check for config file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+mod file;
+
+fn main() -> std::io::Result<()> {
+    println!("dplay");
+
+    if file::check_config() {
+        println!("\nNo config file found. \nCreating a new one");
+        file::create_config();
+
+        println!("\nExiting! Please edit your config file.");
+        return Ok(());
+    } else {
+        println!("\nExisting config file found.");
+    }
+
+    Ok(())
 }
