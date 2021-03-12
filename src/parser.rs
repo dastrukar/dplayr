51a2b72e8b84bf9e98c063435cc2e17d5955d9ae
dastrukar <3derid@gmail.com>
2021-03-12 17:34:46 +0800

    Parser: Add "get_var"

--- src/parser.rs
+++ src/parser.rs
@@ -19,3 +19,26 @@ pub fn get_line_type(text: &String) -> i32 {
     result
 }
 
+/// Returns the variable name and value
+pub fn get_var(text: &String) -> (String, String) {
+    let mut i = 0;
+
+    // Find where the '=' is
+    for c in text.chars() {
+        if c == '=' {
+            break;
+        }
+        i += 1;
+    }
+
+    // If the variable doesn't have a name, panic.
+    if i == 0 { panic!("Invalid variable."); }
+
+    // Check if the variable has a value
+    let length = text.chars().count();
+    println!("{}", length);
+    if length == i + 1 {
+        return (String::from(&text[1..i]), String::from(""));
+    }
+    (String::from(&text[1..i]), String::from(&text[i+1..length]))
+}
