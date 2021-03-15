2a4aaca1dc6846abb437f54e5eecb356acd93f93
dastrukar <3derid@gmail.com>
2021-03-15 20:19:43 +0800

    Parser: Rework get_var... again
    Changes:
    -Removed `get_var_name` and `get_var_value`
    +Brought back `get_var`
    -Removed unused `struct`
    -Removed unused `get_line_type`

--- src/parser.rs
+++ src/parser.rs
@@ -2,80 +2,81 @@
 
 use regex::Regex;
 
-struct Parser;
-
-/// Returns a value based on the line type
-/// `#` : Returns 1
-/// `$` : Returns 2
-/// `:` : Returns 3
-///
-/// If none of the above apply, return 0
-pub fn get_line_type(text: &String) -> i32 {
-    let c = text.chars().nth(0).expect("Failed to load text");
-
-    let result = match c {
-        '#' => 1,
-        '$' => 2,
-        ':' => 3,
-        _ => 0,
-    };
-
-    result
-}
-
 /// Removes all "comments" found in the given string and returns it
 pub fn remove_comments(text: &String) -> String {
-    let re = Regex::new(r"\#.+").expect("Regex error in \"remove_comments\"!");
+    let re = Regex::new(r"\#.+")
+        .expect("Regex error in \"remove_comments\"!");
     let mut result = String::new();
     let mut offset: usize = 0;
 
     // Get anything that isn't a "comment"
     for m in re.find_iter(text) {
-        result.push_str(&text[offset..m.start()].to_string());
+        result.push_str(
+            &text[offset..m.start()]
+            .to_string()
+        );
 
         offset = m.end();
     }
 
     // Get strings that may have been missed
-    result.push_str(&text[offset..text.chars().count()]);
+    result.push_str(
+        &text[
+            offset..text
+                .chars()
+                .count()
+        ]
+        );
 
     result
 }
 
 /// Returns the variable name
-pub fn get_var_name(text: &String) -> Vec<String> {
-    let mut names: Vec<String> = Vec::new();
+pub fn get_var(text: &String) -> (Vec<String>, Vec<String>) {
+    // Get the variables first
+    let re_vars = Regex::new(r"\B[\$[=]].\S+")
+        .expect("Regex error!");
+    let mut vars: Vec<String> = Vec::new();
 
-    let re = Regex::new(r"\B\$.\S+=").expect("Regex error in \"get_var_name\"!");
-
-    // Get "variable name"
-    for m in re.find_iter(text) {
-        let name = m.as_str();
-        let len  = name.chars().count();
-
-        names.push(String::from(name)[1..len-1].to_string());
+    for m in re_vars.find_iter(text) {
+        vars.push(
+            m.as_str()
+            .to_string()
+        );
     }
 
-    names
-}
-
-/// Returns the variable value
-pub fn get_var_value(text: &String) -> Vec<String> {
+    let mut names: Vec<String> = Vec::new();
     let mut values: Vec<String> = Vec::new();
 
-    let re = Regex::new(r"=.?\S*").expect("Regex error in \"get_var_value\"!");
+    let re_names = Regex::new(r"\B\$.\S+=")
+        .expect("Regex error!");
+    let re_values = Regex::new(r"=.?\S*")
+        .expect("Regex error!");
+
+    // Get "variable name"
+    for name in &vars {
+        names.push(
+            re_names.find(&name)
+                .unwrap()
+                .as_str()
+                .to_string()
+        );
+    }
 
     // Get "variable value"
-    for m in re.find_iter(text) {
-        let value = m.as_str();
-        let len   = value.chars().count();
+    for value in &vars {
+        let result = re_values.find(&value)
+            .unwrap()
+            .as_str();
 
-        if len <= 1 {
-            values.push("".to_string());
-        } else {
-            values.push(String::from(value)[1..len].to_string());
-        }
+        values.push(
+            result[
+                1..result
+                    .chars()
+                    .count()
+            ].to_string()
+        );
     }
-    
-    values
+
+    (names, values)
 }
