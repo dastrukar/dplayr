bfda967a4f710624335f1328981194a9f965c0ab
dastrukar <3derid@gmail.com>
2021-03-17 12:14:54 +0800

    File: Use struct "Config" from config.rs

--- src/file.rs
+++ src/file.rs
@@ -5,24 +5,12 @@ use std::fs::File;
 use std::path::Path;
 use std::io::BufReader;
 
+use crate::config::Config;
+
 /// Creates a config file at the current directory.
 pub fn create_config() {
-    fs::write("dplayr.cfg",
-"# This is a comment
-$srcprt=/home/das/Games/gzdoom/gzdoom
-$preset=
-
-# These are \"items\"
-item1
-item2
-item3
-
-start;presetname
-    item4
-    item5
-    item6
-end;")
-        .expect("Couldn't create file.");
+    fs::write("dplayr.cfg", Config::new().text)
+        .expect("Failed to create config file.");
 }
 
 /// Opens "dplayr.cfg" and returns its contents.
@@ -30,10 +18,10 @@ end;")
 pub fn get_config() -> String {
     let file = File::open("dplayr.cfg")
         .expect("Couldn't open config file.");
-    let mut text = String::new();
+    let mut cfg = String::new();
 
-    BufReader::new(file).read_to_string(&mut text).expect("Something went wrong!");
-    text
+    BufReader::new(file).read_to_string(&mut cfg).expect("Something went wrong!");
+    cfg
 }
 
 /// Returns true if dplayr.cfg exists.
