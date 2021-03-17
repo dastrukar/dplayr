db101ae6f7d64644884e810c1f25c6215c5664a1
dastrukar <3derid@gmail.com>
2021-03-17 14:44:57 +0800

    Parser: Change how presets are returned
    Changes:
    +Made constants for RegEx patterns
    +Replaced "re_nowhitespace" with "String::split_whitespace()"
    +Made struct "Vars" variables and functions public
    +Renamed "fetch" functions to "get" functions
    +Organised some functions and stuff
    +Presets now return "Vec<&str>" to allow iterating
    
    Many changes, might miss some.
    Apologies in advance.
    -dastrukar

--- src/parser.rs
+++ src/parser.rs
@@ -2,18 +2,25 @@
 
 use regex::Regex;
 
+const REGEX_PRESETSTART:  &str = r"start;(\S+)?";
+const REGEX_PRESETEND:    &str = r"end;"; 
+const REGEX_VARS:         &str = r"\B\$\S+=(\S+)?";
+const REGEX_NAMES:        &str = r"\B\$\S+=";
+const REGEX_VALUES:       &str = r"=(\S?)+";
+const REGEX_PRESET_SPLIT: &str = r"[^,]+";
+
 
 pub struct Vars<'v> {
-    names:   Vec<&'v str>,
-    values: Vec<&'v str>,
+    pub names:  Vec<&'v str>,
+    pub values: Vec<&'v str>,
 }
 
 impl<'v> Vars<'v> {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Vars { names: Vec::new(), values: Vec::new() }
     }
 
-    fn get_value_of(&self, name: &str) -> Result<&str, ()> {
+    pub fn get_value_of(&self, name: &str) -> Result<&str, ()> {
         let mut vector_index: usize = 0;
         let mut is_exist = false;
 
@@ -32,31 +39,67 @@ impl<'v> Vars<'v> {
 
 
 //=========================================================
-// Fetch functions
-//----------------
+// Remove functions
+//------------------
 
-/// Returns variable declarations from the given `text`
-pub fn fetch_var_dec(text: &String) -> Vec<&str> {
-    let re_vars = Regex::new(r"\B\$\S+=(\S+)?")
-        .expect("Regex error!");
+
+/// Removes all "comments" found in the given `cfg` and returns it
+pub fn remove_comments(cfg: &String) -> String {
+    let re = Regex::new(r"\#.+").unwrap();
+    let mut result = String::new();
+    let mut offset: usize = 0;
+
+    // Get anything that isn't a "comment"
+    for m in re.find_iter(cfg) {
+        result.push_str(
+            &cfg[offset..m.start()]
+            .to_string()
+        );
+
+        offset = m.end();
+    }
+
+    // Get strings that may have been missed
+    result.push_str(
+        &cfg[
+            offset..cfg
+                .chars()
+                .count()
+        ]
+    );
+
+    result
+}
+
+
+//=========================================================
+// Get functions
+//---------------
+
+/// Returns variable declarations from the given `cfg`
+pub fn get_var_declares(cfg: &String) -> Vec<&str> {
+    let re_vars = Regex::new(REGEX_VARS).unwrap();
     let mut vars: Vec<&str> = Vec::new();
 
-    for m in re_vars.find_iter(&text) { vars.push(m.as_str()); }
+    for m in re_vars.find_iter(&cfg) { vars.push(m.as_str()); }
 
     vars
 }
 
 
-pub fn fetch_preset_start_end(name: &str, text: &String) -> Vec<usize> {
-    let re_presetstart = Regex::new(r"start;\S+")
-        .expect("Regex error!");
-    let re_presetend = Regex::new(r"end;")
-        .expect("Regex error!");
+/// Returns all preset names
+
+
+/// Returns `Vec<usize>` of the positions for the given preset `name`
+pub fn get_preset_start_end(name: &str, cfg: &String)
+    -> Result<Vec<usize>, ()> {
+    let re_presetstart = Regex::new(REGEX_PRESETSTART).unwrap();
+    let re_presetend = Regex::new(REGEX_PRESETEND).unwrap();
 
     let mut start_to_end: Vec<usize> = Vec::new();
 
     // Find where the preset params are
-    for start in re_presetstart.find_iter(&text) {
+    for start in re_presetstart.find_iter(&cfg) {
         let len = start
             .as_str()
             .chars()
@@ -67,7 +110,7 @@ pub fn fetch_preset_start_end(name: &str, text: &String) -> Vec<usize> {
         // It's done here due how slicing strings makes static strings
         if start.as_str()[6..len] == name[..] { 
             let end = re_presetend
-                .find_at(&text, start.start())
+                .find_at(&cfg, start.start())
                 .unwrap();
 
             start_to_end
@@ -77,56 +120,46 @@ pub fn fetch_preset_start_end(name: &str, text: &String) -> Vec<usize> {
         }
     }
 
-    start_to_end
-}
-
-//=========================================================
-// Remove functions
-//------------------
-
-/// Removes all "comments" found in the given string and returns it
-pub fn remove_comments(text: &String) -> String {
-    let re = Regex::new(r"\#.+")
-        .expect("Regex error in \"remove_comments\"!");
-    let mut result = String::new();
-    let mut offset: usize = 0;
-
-    // Get anything that isn't a "comment"
-    for m in re.find_iter(text) {
-        result.push_str(
-            &text[offset..m.start()]
-            .to_string()
-        );
-
-        offset = m.end();
+    if start_to_end.len() < 2 {
+        return Err(());
     }
 
-    // Get strings that may have been missed
-    result.push_str(
-        &text[
-            offset..text
-                .chars()
-                .count()
-        ]
-        );
+    Ok(start_to_end)
+}
+
+
+/// Returns parameters from the given preset `name`
+pub fn get_preset_params<'a> (name: &'a str, cfg: &'a String)
+    -> Vec<&'a str> {
+    let range = get_preset_start_end(&name, &cfg)
+        .expect(&format!("\n\n[!!Error while finding presets!!]\nUnable to find preset with the name {:?}.\n\n", name));
+
+    let re_start = Regex::new(REGEX_PRESETSTART).unwrap();
+    let mut result: Vec<&str> = Vec::new();
+
+    let cfg_slice = &cfg[range[0]..range[1]];
+    for item in cfg_slice.split_whitespace() {
+        if !re_start.find(&item).is_none() { 
+            let name = String::from(name);
+            panic!(format!("\n\nUnexpected \"start;\" found inside preset {:?}\n\n", name));
+        }
+        result.push(item);
+    }
 
     result
 }
 
-//=========================================================
-// Get functions
-//---------------
 
 /// Returns variable names and values
-pub fn get_var(text: &String) -> Vars {
+pub fn get_vars(cfg: &String) -> Vars {
     // Get the variables first
-    let variables = fetch_var_dec(&text);
+    let variables = get_var_declares(&cfg);
 
     let mut vars = Vars::new();
 
-    let re_names = Regex::new(r"\B\$\S+=")
+    let re_names = Regex::new(REGEX_NAMES)
         .expect("Regex error!");
-    let re_values = Regex::new(r"=(\S?)+")
+    let re_values = Regex::new(REGEX_VALUES)
         .expect("Regex error!");
 
     // Get "variable name"
@@ -162,17 +195,13 @@ pub fn get_var(text: &String) -> Vars {
     vars
 }
 
+
 /// Returns the parameters from a preset
-pub fn get_preset(text: &String, var: &Vars) -> Result<String, ()> {
-    let preset_var = var.get_value_of("preset");
-    let preset_var = match preset_var {
-        Ok(str) => str,
-        Err(()) => return Err(()),
-    };
+pub fn get_preset<'v> (cfg: &'v String, vars: &'v Vars) -> Result<Vec<&'v str>, ()> {
+    let preset_var = vars.get_value_of("preset")?;
 
     let mut presets: Vec<&str> = Vec::new();
-    let re_split = Regex::new(r"[^,]+")
-        .expect("Regex error!");
+    let re_split = Regex::new(REGEX_PRESET_SPLIT).unwrap();
 
     for preset in re_split
         .find_iter(&preset_var) {
@@ -183,29 +212,11 @@ pub fn get_preset(text: &String, var: &Vars) -> Result<String, ()> {
     }
 
     // Get the preset parameters
-    let mut result = String::new();
+    let mut result: Vec<&str> = Vec::new();
     for preset in &presets {
-        result.push_str(&get_preset_params(preset, text));
-        println!("h");
+        result.append(&mut get_preset_params(preset, cfg));
     }
 
     println!("$preset={:?}", presets);
     Ok(result)
-}
-
-/// Returns parameters from the given preset `name`
-pub fn get_preset_params<'a> (name: &str, text: &String) -> String {
-    let range = fetch_preset_start_end(&name, &text);
-    let re_nowhitespace = Regex::new(r"\S+")
-        .expect("Regex error!");
-    let mut result = String::new();
-
-    let text_slice = &text[range[0]..range[1]];
-    for item in re_nowhitespace
-        .find_iter(&text_slice) {
-            result.push_str(" ");
-            result.push_str(item.as_str());
-    }
-
-    result
 }
