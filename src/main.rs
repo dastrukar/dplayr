c07956e9fa63af14bd70b5429b76d28fc8efef83
dastrukar <3derid@gmail.com>
2021-03-18 19:36:33 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 mod file;
+mod parser;
+mod config;
 
-fn main() -> std::io::Result<()> {
-    println!("dplay");
+use std::io::Result;
+use std::process::Command;
 
-    if file::check_config() {
+use crate::parser::Vars;
+
+fn main() -> Result<()> {
+
+    if !file::check_config() {
         println!("\nNo config file found. \nCreating a new one");
         file::create_config();
 
@@ -13,5 +19,31 @@ fn main() -> std::io::Result<()> {
         println!("\nExisting config file found.");
     }
 
+    let config: String = parser::remove_comments(&file::get_config());
+    let vars:   Vars   = parser::get_vars(&config);
+
+    println!("Variables:\n{:?}\n\nValues:\n{:?}", vars.names, vars.values);
+
+    let parameters: Vec<String> = parser::get_parameters(&config, &vars);
+    println!("{:?}", parameters);
+
+    let presets = parser::get_preset(&config, &vars);
+    let presets: Vec<String> = match presets {
+        Ok(str) => str,
+        Err(()) => Vec::new(),
+    };
+
+    println!("{:?}", presets);
+
+
+    // Run the game
+    let run = vars.get_value_of("srcprt")
+        .expect("\n\nCouldn't find variable \"srcprt\".\n\n");
+
+    Command::new(&run)
+        .args(&parameters)
+        .args(&presets)
+        .status()
+        .expect(&format!("\n\nFailed to execute process.\nPerhaps the directory given was incorrect?\n\nCommand:{:?}{:?}\n\n", run, presets));
     Ok(())
 }
